2b3d12b4893bc14c6a279de57e7aa73c7844bae3
guangjh <guangjinhai@163.com>
2021-03-21 08:07:37 +0800

    finalize obj relive

---
+++ src/main/java/com/gjh/learn/jvm/CanReliveObj.java
@@ -0,0 +1,38 @@
+package com.gjh.learn.jvm;
+
+import com.gjh.learn.jvm.utils.Print;
+
+/**
+ * created on 2021/3/21
+ *
+ * @author kevinlights
+ */
+public class CanReliveObj {
+    public static CanReliveObj obj;
+
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+        Print.PURPLE("CanReliveObj finalize called");
+        obj = this;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        obj = new CanReliveObj();
+
+        doGc();
+        Print.BLUE("second gc");
+        doGc();
+    }
+
+    private static void doGc() throws InterruptedException {
+        obj = null;
+        System.gc();
+        Thread.sleep(1000);
+        if (obj == null) {
+            Print.RED("obj is null");
+        } else {
+            Print.GREEN("obj is available");
+        }
+    }
+}
