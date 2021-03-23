428c8c64369a3e015949fb85009e65626d7563cb
guangjh <guangjinhai@163.com>
2021-03-23 08:03:09 +0800

    gc tenuring threshold

---
+++ src/main/java/com/gjh/learn/jvm/AllocEden.java
@@ -0,0 +1,16 @@
+package com.gjh.learn.jvm;
+
+/**
+ * created on 2021/3/23
+ *
+ * @author kevinlights
+ */
+public class AllocEden {
+    public static final int _1K = 1024;
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 5 * _1K; i++) {
+            byte[] b = new byte[_1K];
+        }
+    }
+}
