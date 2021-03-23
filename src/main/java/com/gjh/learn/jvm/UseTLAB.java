79e2c027bb04da3219d4b2cd9529d13555f572be
guangjh <guangjinhai@163.com>
2021-03-23 08:39:15 +0800

    TLAB

---
+++ src/main/java/com/gjh/learn/jvm/UseTLAB.java
@@ -0,0 +1,23 @@
+package com.gjh.learn.jvm;
+
+import com.gjh.learn.jvm.utils.Print;
+
+/**
+ * created on 2021/3/23
+ *
+ * @author kevinlights
+ */
+public class UseTLAB {
+    public static void alloc() {
+        byte[] b = new byte[2];
+        b[0] = 1;
+    }
+
+    public static void main(String[] args) {
+        long b = System.currentTimeMillis();
+        for (int i = 0; i < 10000000; i++) {
+            alloc();
+        }
+        Print.GREEN(System.currentTimeMillis() - b);
+    }
+}
