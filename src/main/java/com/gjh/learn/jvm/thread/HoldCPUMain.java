9ecb938c4f4ef5a7004d79275cf2397c62ba964a
guangjh <guangjinhai@163.com>
2021-03-25 08:49:35 +0800

    thread monitor

---
+++ src/main/java/com/gjh/learn/jvm/thread/HoldCPUMain.java
@@ -0,0 +1,37 @@
+package com.gjh.learn.jvm.thread;
+
+/**
+ * created on 2021/3/25
+ *
+ * @author kevinlights
+ */
+public class HoldCPUMain {
+    public static class HoldCPUTask implements Runnable {
+        @Override
+        public void run() {
+            while (true) {
+                double a = Math.random() * Math.random();// 占用 CPU
+            }
+        }
+    }
+
+    public static class LazyTask implements Runnable {
+        @Override
+        public void run() {
+            try {
+                while (true) {
+                    Thread.sleep(1000);// 空闲线程
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new Thread(new HoldCPUTask()).start();
+        new Thread(new LazyTask()).start();
+        new Thread(new LazyTask()).start();
+        new Thread(new LazyTask()).start();
+    }
+}
---
+++ src/main/java/com/gjh/learn/jvm/thread/HoldLockMain.java
@@ -0,0 +1,50 @@
+package com.gjh.learn.jvm.thread;
+
+import java.util.Random;
+
+/**
+ * created on 2021/3/25
+ *
+ * @author kevinlights
+ */
+public class HoldLockMain {
+    public static Object[] lock = new Object[10];
+    public static Random r = new Random();
+
+    static {
+        for (int i = 0; i < lock.length; i++) {
+            lock[i] = new Object();
+        }
+    }
+
+    public static class HoldLockTask implements Runnable {
+        private int i;
+
+        public HoldLockTask(int i) {
+            this.i = i;
+        }
+
+        @Override
+        public void run() {
+            try {
+                while (true) {
+                    synchronized (lock[i]) {
+                        if (i % 2 == 0) {
+                            lock[i].wait(r.nextInt(10));
+                        } else {
+                            lock[i].notifyAll();
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < lock.length * 2; i++) {
+            new Thread(new HoldLockTask(i / 2)).start();
+        }
+    }
+}
